f0397e4484f3d959df2f4eca8adf04dfb34b6b28
Mistyttm <emmey.leo@gmail.com>
2023-06-02 19:33:39 +1000

    Completed searching endpoint

--- moviesAPI/app.js
+++ moviesAPI/app.js
@@ -1,26 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+const options = require("./knexfile.js");
+const { attachPaginate } = require("knex-paginate");
+const knex = require("knex")(options);
+attachPaginate();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  req.db = knex;
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.get("/knex", function(req, res, next) {
+  req.db
+    .raw("SELECT VERSION()")
+    .then(version => console.log(version[0][0]))
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+  res.send("Version Logged successfully");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +50,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- moviesAPI/routes/index.js
+++ moviesAPI/routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/movies/search", function(req, res, next) {
+  const { title, year, page } = req.query;
+  const query = req.db
+    .from("basics")
+    .select(
+      "primaryTitle AS title",
+      "year",
+      "tconst AS imdbID",
+      "imdbRating",
+      "rottentomatoesRating",
+      "metacriticRating",
+      "rated AS classification"
+    );
+
+  if (title !== undefined) {
+    query.where("primaryTitle", "LIKE", `%${title}%`);
+  }
+
+  if (year !== undefined) {
+    if (!/^\d{4}$/.test(year)) {
+      return res.json({ Error: true, Message: "Invalid year format. Please use 'yyyy' format." });
+    }
+    query.where("year", "LIKE", `%${year}%`);
+  }
+
+  query.paginate({
+      perPage: 100,
+      currentPage: page || 1,
+      isLengthAware: true
+    })
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+    });
 });
 
 module.exports = router;
